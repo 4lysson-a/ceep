ae37cd36f4cdb8042144a320ab15a1132baacfbe
4ly <alyssonalcantaralves@gmail.com>
2020-12-22 00:25:00 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ListaNotas from "./components/ListaNotas";
+import Titulo from "./components/Titulo";
+import FormularioCad from "./components/FormularioCad";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './app.css'
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <Titulo/>
+        <FormularioCad />
+        <ListaNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
