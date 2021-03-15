1f8b56b024acf1704cea2bd4628120b8dfa2f1be
4ly-a <4ly.alcantara@gmail.com>
2021-03-15 16:11:06 -0300

    constructor

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import ListaNotas from "./components/ListaNotas";
 import Titulo from "./components/Titulo";
 import FormularioCad from "./components/FormularioCad";
 
-import './app.css'
+import "./app.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      notas: [],
+    };
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas: novoArrayNotas,
+    };
+    this.setState(novoEstado);
+  }
+
   render() {
     return (
       <section>
-        <Titulo/>
-        <FormularioCad />
-        <ListaNotas />
+        <Titulo />
+        <FormularioCad criarNota={this.criarNota.bind(this)} />
+        <ListaNotas notas={this.state.notas} />
       </section>
     );
   }
