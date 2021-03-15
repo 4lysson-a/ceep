fe5805e46a2eaf9d401d88d206139e92dff935c0
4ly-a <4ly.alcantara@gmail.com>
2021-03-15 16:11:38 -0300

    form card

--- src/components/FormularioCad.jsx
+++ src/components/FormularioCad.jsx
@@ -1,22 +1,53 @@
 import React, { Component } from "react";
 import "./StyleForm.css";
 
-export default class FormularioCad extends Component {
+class FormularioCad extends Component {
+  constructor(props) {
+    super(props);
+    this.titulo = "";
+    this.texto = "";
+  }
+
+  _handleMudancaTitulo(evento) {
+    evento.stopPropagation();
+    this.titulo = evento.target.value;
+  }
+
+  _handleMudancaTexto(evento) {
+    evento.stopPropagation();
+    this.texto = evento.target.value;
+  }
+
+  _criarNota(evento) {
+    evento.preventDefault(); // prevenir o comportamento padrão
+    evento.stopPropagation(); // pare sua propagação
+    this.props.criarNota(this.titulo, this.texto);
+  }
+
   render() {
     return (
       <div className="form-container">
-        <form action="">
-          <input className="nota-titulo" placeholder="Título" type="text" />
+        <form onSubmit={this._criarNota.bind(this)}>
+          <input
+            className="nota-titulo"
+            placeholder="Título"
+            type="text"
+            onChange={this._handleMudancaTitulo.bind(this)}
+          />
+
           <textarea
             placeholder="Escreva sua nota aqui ..."
             name="description"
             id="description"
             cols="40"
             rows="10"
+            onChange={this._handleMudancaTexto.bind(this)}
           ></textarea>
+          <button className="b-criar-nota">Criar Nota</button>
         </form>
-        <button className="b-criar-nota">Criar Nota</button>
       </div>
     );
   }
 }
+
+export default FormularioCad;
