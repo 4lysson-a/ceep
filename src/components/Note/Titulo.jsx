c09aeb5e91788c5202994407ba0fbd9c68e65c23
4ly-a <4ly.alcantara@gmail.com>
2021-03-16 18:47:32 -0300

    title

---
+++ src/components/Note/Titulo.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "../../index.css";
+
+class Titulo extends Component {
+  render() {
+    return (
+      <section>
+        <h1 className="app-title">
+          <span className="c">C</span>
+          <span className="e">e</span>
+          <span className="e2">e</span>
+          <span className="p">p</span>
+        </h1>
+        <p className="slogan">Anotações Simplificadas :)</p>
+      </section>
+    );
+  }
+}
+
+export default Titulo;
