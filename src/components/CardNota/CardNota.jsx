780952069b6a5db5d6e9d502ed810da5c053b9be
4ly-a <4ly.alcantara@gmail.com>
2021-03-16 12:15:48 -0300

    delete card

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
-
+import Delete from "../../img/delete.svg";
 class CardNota extends Component {
+  apagar() {
+    const indice = this.props.indice;
+    this.props.apagarNota(indice);
+  }
+
   render() {
     return (
-      <section className="card-nota"> 
+      <section className="card-nota">
+        <img
+          className="delete-icon"
+          src={Delete}
+          onClick={this.apagar.bind(this)}
+        />
         <header>
           <h3>{this.props.titulo}</h3>
         </header>
