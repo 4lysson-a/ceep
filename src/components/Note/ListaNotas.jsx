d4315d2af3965a74fd97c1089eb551c2b65e712d
4ly-a <4ly.alcantara@gmail.com>
2021-03-16 18:47:29 -0300

    note list

---
+++ src/components/Note/ListaNotas.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import CardNota from "../CardNota/CardNota";
+
+import "../CardNota/StyleCardNota.css";
+
+class ListaNotas extends Component {
+  render() {
+    return (
+      <section className="card-container">
+        <ul>
+          {this.props.notas.map((nota, index) => {
+            return (
+              <li key={index}>
+                <CardNota
+                  indice={index}
+                  apagarNota={this.props.apagarNota}
+                  titulo={nota.titulo}
+                  texto={nota.texto}
+                  categoria={nota.categoria}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    );
+  }
+}
+
+export default ListaNotas;
